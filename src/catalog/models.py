5e10db6d860ef14cf709031867782a54719621c0
ArthurGsn <gsn.arthur@icloud.com>
2022-03-07 17:02:28 +0100

    Début code models.py

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Furnitures (models.Model):
+    TYPE_CHOICES = [
+        ('Bed'),
+        ('Chair'),
+        ('Table'),
+        ('Couch'),
+        ('Closet'),
+    ]
+    type = models.CharField(
+        max_length=10,
+        choices=TYPE_CHOICES,
+    )
+
+    price = models.FloatField
+    COLOR_CHOICES = [
+        ('Black'),
+        ('White'),
+        ('Grey'),
+        ('Brown'),
+        ('Yellow'),
+        ('Blue'),
+        ('Green'),
+        ('Red'),
+        ('Pink'),
+        ('Purple'),
+    ]
+    color = models.CharField(
+        max_length=10,
+        choices=COLOR_CHOICES,
+    )
+    CONDITION_CHOICES = [
+        ('New'),
+        ('Good'),
+        ('Used'),
+        ('Old'),
+    ]
+    condition = models.CharField(
+        max_length=10,
+        choices=CONDITION_CHOICES,
+    )
+    MATERIAL_CHOICES = [
+        ('Wood'),
+        ('Metal'),
+        ('Plastic'),
+        ('Leather'),
+        ('Wool'),
+
+    ]
+    material = models.CharField(
+        max_length=10,
+        choices=MATERIAL_CHOICES,
+    )
+    dimension = models.CharField
+
+    stock = models.BooleanField
+
+
