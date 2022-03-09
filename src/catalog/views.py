de2fbd2326d7311d3f9db8977841a8d25bb446b2
Alexia <alexiatgh@gmail.com>
2022-03-09 16:47:52 +0100

    Show database
    Before: Used to show only one element
    Now: Show all of them

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -5,14 +5,20 @@ import json
 
 def furnitures(request):
     data = Furnitures.objects.all() #Récupère tous les enregistrements du tableau
-    params = {
-            'price':str(data[0].price), #Puisque chaque enregistrement est de type data, transtypez-le en type String
-            'dimension':str(data[0].dimension),
-            'stock':bool(data[0].stock),
-            'type':str(data[0].type),
-            'color':str(data[0].color),
-            'condition':str(data[0].condition),
-            'material':str(data[0].material),
+    
+    furnitures = []
+    for furniture in data:
+        params = {
+            'price':str(furniture.price), #Transformation de chaque enregistement de type data (données) en type String(chaine de caractères)
+            'dimension':str(furniture.dimension),
+            'stock':bool(furniture.stock),
+            'type':str(furniture.type),
+            'color':str(furniture.color),
+            'condition':str(furniture.condition),
+            'material':str(furniture.material),
         }
-    json_str = json.dumps(params, ensure_ascii=False, indent=2)
+        furnitures.append(params)
+    
+    #Retourne la transformation du dessus en format json sans caractères spéciaux (ensure_ascii) avec une indentation de 2 au lieu de 4(par défaut)
+    json_str = json.dumps(furnitures, ensure_ascii=False, indent=2) 
     return HttpResponse(json_str)
