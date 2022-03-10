0e278efc3b3d9b80e232bc63eb440deb8025e68f
Zniber Amine <zniberfr@gmail.com>
2022-03-10 16:21:13 +0100

    API Clients
    Createe API clients

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Clients (models.Model):
+    id = models.AutoField(primary_key=True)
+    pseudo = models.CharField(max_length=10 , default='Choose an alias')
+    email = models.CharField(max_length=35 ,default='Add your e-mail here')
+    #password = models.CharField#
+    zipcode = models.IntegerField(default='Add your zip code')
+    bio = models.TextField(max_length=280, default='Add a bio')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,36 @@
+from django import urls
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Clients #Importer la classe Clients
+import json
+def clients(request):
+    data = Clients.objects.all() #Récupère tous les enregistrements du tableau
+    clients = []
+    for client in data:
+        params = {
+            'id':int(client.id),
+            'pseudo' : str(client.pseudo),
+            'email' :str(client.email),
+            'zipcode' :int(client.zipcode),
+            'bio':str(client.bio), #Transformation de chaque enregistement de type data (données) en type String(chaine de caractères)
+            #'password':str(client.password),#
+        }
+        clients.append(params)
+    #Retourne la transformation du dessus en format json sans caractères spéciaux (ensure_ascii) avec une indentation de 2 au lieu de 4(par défaut)
+    json_str = json.dumps(clients, ensure_ascii=False, indent=2)
+    return HttpResponse(json_str)
 
-# Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
